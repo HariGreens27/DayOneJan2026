ba47834504bdca591b3d11381f0adf580626969e
HariGreens27 <harigreenstech07@gmail.com>
2026-01-22 15:41:21 +0530

    p2 2nd time

--- src/org/data/DataBase.java
+++ src/org/data/DataBase.java
@@ -3,7 +3,7 @@ package org.data;
 public class DataBase {
 	public static void main(String[] args) {
 		System.out.println("jack");
-		System.out.println("max");
+		System.out.println("james");
                 System.out.println("Ram");
 	}
 
